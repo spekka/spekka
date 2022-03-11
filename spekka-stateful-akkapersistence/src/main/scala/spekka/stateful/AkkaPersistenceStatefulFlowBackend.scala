f91aee2fb1f5fc301ea57c1f6e10899eef556d76
Andrea Zito <zito.andrea@gmail.com>
2022-03-11 21:03:47 +0100

    Scalafix + scalafmt + license headers

--- spekka-stateful-akkapersistence/src/main/scala/spekka/stateful/AkkaPersistenceStatefulFlowBackend.scala
+++ spekka-stateful-akkapersistence/src/main/scala/spekka/stateful/AkkaPersistenceStatefulFlowBackend.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 Andrea Zito
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package spekka.stateful
 
 import akka.actor.typed.ActorRef
@@ -48,8 +64,7 @@ object AkkaPersistenceStatefulFlowBackend {
   }
 
   object StateWrapper {
-    def recoveredState[State](s: State): StateWrapper[State] =
-      new StateWrapper(true, false, s)
+    def recoveredState[State](s: State): StateWrapper[State] = new StateWrapper(true, false, s)
   }
 
   class SerializedDataSnapshotAdapter[T](codec: Codec[T]) extends SnapshotAdapter[T] {
@@ -340,7 +355,7 @@ object AkkaPersistenceStatefulFlowBackend {
         storagePlugin: EventBased.PersistencePlugin,
         retentionCriteria: RetentionCriteria = RetentionCriteria.disabled,
         eventsPartitions: Int = 1,
-        sideEffectsParallelism: Int = 1,
+        sideEffectsParallelism: Int = 1
       ): EventBased[State, Ev] =
       new EventBased(
         storagePlugin,
--- spekka-stateful-akkapersistence/src/test/scala/spekka/stateful/AkkaPersistenceStatefulFlowSuite.scala
+++ spekka-stateful-akkapersistence/src/test/scala/spekka/stateful/AkkaPersistenceStatefulFlowSuite.scala
@@ -1,27 +1,43 @@
+/*
+ * Copyright 2022 Andrea Zito
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package spekka.stateful
 
-import spekka.test.SpekkaSuite
+import akka.actor.typed.ActorSystem
+import akka.persistence.testkit.PersistenceTestKitDurableStateStorePlugin
 import akka.persistence.testkit.PersistenceTestKitPlugin
 import akka.persistence.testkit.PersistenceTestKitSnapshotPlugin
-import com.typesafe.config.ConfigFactory
-import akka.actor.typed.ActorSystem
-
-import scala.concurrent.duration._
-import akka.stream.scaladsl.Source
-import akka.stream.scaladsl.Keep
-import akka.stream.scaladsl.Sink
-import akka.persistence.typed.scaladsl.RetentionCriteria
-import spekka.codec.Encoder
-import spekka.codec.Decoder
-import scala.util.Success
-import scala.util.Failure
 import akka.persistence.testkit.scaladsl.PersistenceTestKit
-import org.scalatest.BeforeAndAfterEach
 import akka.persistence.testkit.scaladsl.SnapshotTestKit
 import akka.persistence.typed.PersistenceId
-import akka.persistence.testkit.PersistenceTestKitDurableStateStorePlugin
+import akka.persistence.typed.scaladsl.RetentionCriteria
+import akka.stream.scaladsl.Keep
+import akka.stream.scaladsl.Sink
+import akka.stream.scaladsl.Source
 import akka.testkit.TestProbe
+import com.typesafe.config.ConfigFactory
+import org.scalatest.BeforeAndAfterEach
+import spekka.codec.Decoder
+import spekka.codec.Encoder
+import spekka.test.SpekkaSuite
+
 import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.util.Failure
+import scala.util.Success
 
 object AkkaPersistenceStatefulFlowSuite {
   val config = ConfigFactory
